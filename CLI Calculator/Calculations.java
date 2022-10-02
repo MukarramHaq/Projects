b6c0228c501b228a14d7b588e01636ffcf2284c8
MukarramHaq <44123470+MukarramHaq@users.noreply.github.com>
2022-10-02 15:00:08 -0400

    Add files via upload

---
+++ CLI Calculator/Calculations.java
@@ -0,0 +1,41 @@
+public class Calculations {
+
+    double sum(double a, double b){
+        double addition = 0;
+
+        addition = a + b;
+
+        return addition;
+    }
+
+    double division(double a, double b){
+
+        double division = 0;
+
+        division = a / b;
+
+        return division;
+
+    }
+
+    double multiplication(double a, double b){
+
+        double multiplication = 0;
+
+        multiplication = a * b;
+
+        return multiplication;
+
+    }
+
+    double subtraction(double a, double b){
+
+        double subtraction = 0;
+
+        subtraction = a - b;
+
+        return subtraction;
+
+    }
+
+}
